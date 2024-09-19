bb16dae39cfc75f8c3b7ddf7468d466904e4cea7
Michal Mocarski <>
2024-09-19 10:55:48 +0200

    [TASK-3] Using query strings to pass parameters instead of the request body

--- pdf-creator-nestjs-server/src/pdf-creator/pdf-creator.controller.ts
+++ pdf-creator-nestjs-server/src/pdf-creator/pdf-creator.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { PdfCreatorService } from './pdf-creator.service';
 import { PdfRequestDto } from './dto/pdf-request.dto';
 
@@ -7,7 +7,7 @@ export class PdfCreatorController {
     constructor(private readonly pdfCreatorService: PdfCreatorService) { }
 
     @Get('create')
-    createPdf(@Body() pdfRequestDto: PdfRequestDto): string {
+    createPdf(@Query() pdfRequestDto: PdfRequestDto): string {
         return this.pdfCreatorService.createPdf(pdfRequestDto);
     }
 }
